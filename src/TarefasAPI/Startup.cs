352139b3201ada19fa8aedda9d9697860b066850
Caio Pontalti <caiopontalti@hotmail.com>
2020-03-19 18:20:36 -0300

    finalizando teste inicial Git

--- src/TarefasAPI/Startup.cs
+++ src/TarefasAPI/Startup.cs
@@ -32,10 +32,6 @@ namespace TarefasAPI
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            //TESTE
-
-
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
