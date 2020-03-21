ab46fc5547620c7c4ff6e24fd3b8fde0e610a4bb
Caio Pontalti <caiopontalti@hotmail.com>
2020-03-21 09:13:44 -0300

    continuacao

--- src/TarefasAPI/Controllers/UsuarioController.cs
+++ src/TarefasAPI/Controllers/UsuarioController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using TarefasAPI.DTO;
 using TarefasAPI.Interfaces;
 using TarefasAPI.Models;
@@ -18,12 +24,15 @@ namespace TarefasAPI.Controllers
         private readonly IUsuarioRepositorio _usuarioRepositorio;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
 
-        public UsuarioController(IUsuarioRepositorio usuarioRepositorio, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager,
+                                 IConfiguration configuration)
         {
             _usuarioRepositorio = usuarioRepositorio;
             _signInManager = signInManager;
             _userManager = userManager;
+            _configuration = configuration;
         }
 
         [HttpPost("login")]
@@ -37,21 +46,30 @@ namespace TarefasAPI.Controllers
                 if (usuario != null)
                 {
                     await _signInManager.SignInAsync(usuario, false);
-                    return Ok();
+                    return Ok(await GerarToken(usuarioDTO.Email));
                 }
-                else 
+                else
                 {
                     return NotFound("Usuário ou Senha inválidos");
                 }
             }
-            else 
+            else
             {
                 return UnprocessableEntity(ModelState);
             }
         }
 
+        [Authorize]
+        [HttpGet("logoff")]
+        public async Task<IActionResult> Logoff()
+        {
+            await _signInManager.SignOutAsync();
+
+            return Ok();
+        }
+
         [HttpPost("cadastrar")]
-        public async Task<IActionResult> Cadastrar ([FromBody] UsuarioDTO usuario)
+        public async Task<IActionResult> Cadastrar([FromBody] UsuarioDTO usuario)
         {
             if (ModelState.IsValid)
             {
@@ -65,20 +83,50 @@ namespace TarefasAPI.Controllers
                 {
                     return Ok();
                 }
-                else 
+                else
                 {
                     List<string> Erros = new List<string>();
                     foreach (var erro in result.retorno)
                         Erros.Add(erro.Description);
 
                     return UnprocessableEntity(Erros);
-                }    
+                }
             }
-            else 
+            else
             {
                 return UnprocessableEntity(ModelState);
             }
-                
+
+        }
+
+        private async Task<object> GerarToken(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            List<Claim> claims = new List<Claim>();
+
+            
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())); 
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, DateTime.UtcNow.ToString())); 
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString(), ClaimValueTypes.Integer64)); 
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["AppSettings:Secret"]);
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Subject = identityClaims, 
+                Issuer = _configuration["AppSettings:Emissor"],
+                Audience = _configuration["AppSettings:ValidoEm"], 
+                Expires = DateTime.UtcNow.AddHours(Convert.ToDouble(_configuration["AppSettings:ExpiracaoHoras"])),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+            return new { Token = encodedToken, Exp = DateTime.UtcNow.AddHours(Convert.ToDouble(_configuration["AppSettings:ExpiracaoHoras"])) };
         }
     }
 }
--- src/TarefasAPI/Startup.cs
+++ src/TarefasAPI/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -11,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using TarefasAPI.Database;
 using TarefasAPI.Interfaces;
 using TarefasAPI.Models;
@@ -42,6 +46,37 @@ namespace TarefasAPI
                 opt.UseSqlServer(_configuration.GetConnectionString("conn"));
             });
 
+            var key = Encoding.ASCII.GetBytes(_configuration["AppSettings:Secret"]);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; //Toda autenticação é para gerar token
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; //Todo request verifica se está autenticado
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false; //apenas https
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = _configuration["AppSettings:ValidoEm"],
+                    ValidIssuer = _configuration["AppSettings:Emissor"],
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .Build()
+                );
+            });
+
+
             services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
             services.AddScoped<ITarefaRepositorio, TarefaRepositorio>();
 
@@ -49,14 +84,16 @@ namespace TarefasAPI
 
             services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<TarefasContext>();
 
-            //services.ConfigureApplicationCookie(opt =>
-            //{
-            //    opt.Events.OnRedirectToLogin = context =>
-            //    {
-            //        context.Response.StatusCode = 401;
-            //        return Task.CompletedTask;
-            //    };
-            //});
+
+            //Opção caso o Authorize não esteja funcionando  
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.Events.OnRedirectToLogin = context =>
+                {
+                    context.Response.StatusCode = 401;
+                    return Task.CompletedTask;
+                };
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
